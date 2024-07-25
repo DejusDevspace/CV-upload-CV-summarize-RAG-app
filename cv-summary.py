1bfcd54a7e36c805250b303cee7bf0f319fa10b2
Deju Adejo <ojomideju2003@gmail.com>
2024-07-25 10:47:28 +0100

    testing

--- cv-summary.py
+++ cv-summary.py
@@ -5,6 +5,7 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains.summarize import load_summarize_chain
 import streamlit as st
 import io
+import time
 from dotenv import load_dotenv
 
 # Loads environment variables
@@ -19,20 +20,29 @@ def process_docx(docx_file: io.IOBase):
     pass
 
 
+def stream_data(text: str):
+    for word in text.split(' '):
+        yield word + ' '
+        time.sleep(0.07)
+
+
 def main():
     # Title of the streamlit page
     st.title('CV Summary Generator')
     # File uploader to upload cv in either docx or pdf formats
     uploaded_file = st.file_uploader('Upload CV', type=['docx', 'pdf'])
     
-    if uploaded_file is not None:
-        file_type = uploaded_file.name.split('.')[-1] #(docx/pdf)
-        if file_type == 'docx':
-            pass
-        elif file_type == 'pdf':
-            pass
-        else:
-            st.error('Unsupported file format! Please upload a .pdf or .docx file.')
+    # Summarize button
+    if st.button('Summarize'):
+        if uploaded_file is not None:
+            file_type = uploaded_file.name.split('.')[-1] #(docx/pdf)
+            if file_type == 'docx':
+                st.write_stream(stream_data('I am a boy that is testing lorem ipsum kinda shit here'))
+                # st.text_st('Docx file!')
+            elif file_type == 'pdf':
+                st.text('PDF File!')
+            else:
+                st.error('Unsupported file format! Please upload a .pdf or .docx file.')
 
 
 if __name__ == '__main__':
