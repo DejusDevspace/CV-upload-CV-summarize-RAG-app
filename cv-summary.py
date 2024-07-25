b0041540e75a2ab2213ea165c4c2f2e700cddafb
Deju Adejo <ojomideju2003@gmail.com>
2024-07-25 10:06:53 +0100

    start_

--- cv-summary.py
+++ cv-summary.py
@@ -1,2 +1,19 @@
+from langchain_google_genai import GoogleGenerativeAI
+from langchain.document_loaders import PyPDFLoader, Docx2txtLoader
+from langchain.prompts import PromptTemplate
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains.summarize import load_summarize_chain
+import streamlit as st
+from dotenv import load_dotenv
+
+# Loads environment variables
+load_dotenv()
 
 
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
