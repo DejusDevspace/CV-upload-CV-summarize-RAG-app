dbb2c343049e5f04557f17356e77a94bb3be7508
Deju Adejo <ojomideju2003@gmail.com>
2024-07-25 13:44:11 +0100

    debugging

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from langchain.chains.summarize import load_summarize_chain
 import streamlit as st
 import io
 import time
+from typing import List
+import tempfile
+import os
 from dotenv import load_dotenv
 
 # Loads environment variables
@@ -13,7 +16,7 @@ load_dotenv()
 
 
 # Function to process .pdf files
-def process_pdf(pdf_file):
+def process_pdf(pdf_file: str) -> List:
     text = ''
     pdf_loader = PyPDFLoader(pdf_file)
 
@@ -37,17 +40,24 @@ def process_pdf(pdf_file):
 
     return texts
     
-
-def process_docx(docx_file):
-    texts = ''
+# Function to process .docx files
+def process_docx(docx_file: str) -> List:
+    text = ''
     docx_loader = Docx2txtLoader(docx_file)
 
     # Load Documents and split into chunks
-    texts = docx_loader.load_and_split()
+    text = docx_loader.load_and_split()
 
-    return texts
+    return text
 
 
+def load_file(file: io.IOBase, suffix: str) -> str:
+    # Save uploaded file to a temporary file and get the path
+    with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as tmp_file:
+        tmp_file.write(file.getbuffer())
+        file_path = tmp_file.name
+    return file_path
+    
 def stream_data(text: str):
     for word in text.split(' '):
         yield word + ' '
@@ -70,62 +80,76 @@ def main():
             st.write(f'File Name: {uploaded_file.name}')
             st.write(f'File Type: {'Word (.docx)' if file_type == 'docx' else 'PDF (.pdf)'}')
 
-            if file_type == 'docx':
-                texts = process_docx(uploaded_file.name)
-            elif file_type == 'pdf':
-                texts = process_pdf(uploaded_file.name)
-            else:
-                st.error('Unsupported file format! Please upload a .pdf or .docx file.')
+            # Get the temporary path to the file
+            file_path = load_file(uploaded_file, suffix=f'.{file_type}')
+
+            try:
+                if file_type == 'docx':
+                    texts = process_docx(file_path)
+                elif file_type == 'pdf':
+                    texts = process_pdf(file_path)
+                else:
+                    st.error('Unsupported file format! Please upload a .pdf or .docx file.')
+            finally:
+                # Remove the temporary file after getting the data from it
+                os.remove(file_path)
     
-    # LLM object
-    llm = GoogleGenerativeAI(model='gemini-1.5-pro', temperature=0)
+            # LLM object
+            llm = GoogleGenerativeAI(model='gemini-1.5-pro', temperature=0)
 
-    # Prompt template
-    prompt_template = """You are given a resume to summarize.
-    Write a verbose detail of the following:
-    {text}
+            # Prompt template
+            prompt_template = """You are given a resume to summarize.
+            Write a verbose detail of the following:
+            {text}
 
-    Details:
-    """
-    # Prompt
-    prompt = PromptTemplate.from_template(prompt_template)
+            Details:
+            """
+            # Prompt
+            prompt = PromptTemplate.from_template(prompt_template)
 
-    # Refine chain prompt template
-    refine_template = (
-           "Your job is to produce a final summary\n"
-           "We have provided an existing summary up to a certain point: {existing_answer}\n"
-           "We want a refined version of the existing summary (if needed) based on the additional context below\n"
-           "------------\n"
-           "{text}\n"
-           "------------\n"
-           "Given the new context, refine the original summary into the following sections:"
-           "Name: \n"
-           "Email: \n"
-           "Key Skills: \n"
-           "Last Company: \n"
-           "Experience Summary: \n"
+            # Refine chain prompt template
+            refine_template = (
+                   "Your job is to produce a final summary\n"
+                   "We have provided an existing summary up to a certain point: {existing_answer}\n"
+                   "We want a refined version of the existing summary (if needed) based on the additional context below\n"
+                   "------------\n"
+                   "{text}\n"
+                   "------------\n"
+                   "Given the new context, refine the original summary into the following sections:"
+                   "Name: \n"
+                   "Email: \n"
+                   "Key Skills: \n"
+                   "Last Company: \n"
+                   "Experience Summary: \n"
 
-           "If the provided context is not useful, return the original summary\n"
-           "If any of the sections are not retrievable from the context, say it is not available in the document\n"
-           "For example, if Last Company is not available, you would write in the Last Company section:\n"
-           "Last Company: Not avaialalbe\n"
-       )
-    refine_prompt = PromptTemplate.from_template(refine_template)
+                   "If the provided context is not useful, return the original summary\n"
+                   "If any of the sections are not retrievable from the context, say it is not available in the document\n"
+                   "For example, if Last Company is not available, you would write in the Last Company section:\n"
+                   "Last Company: Not avaialalbe\n"
+               )
+            refine_prompt = PromptTemplate.from_template(refine_template)
 
-    chain = load_summarize_chain(
-        llm=llm,
-        chain_type='refine',
-        question_prompt=prompt,
-        refine_prompt=refine_prompt,
-        return_intermediate_steps=True,
-        input_key='input_documents',
-        output_key='output_text',
-    )
+            # Summarize chain
+            chain = load_summarize_chain(
+                llm=llm,
+                chain_type='refine',
+                question_prompt=prompt,
+                refine_prompt=refine_prompt,
+                return_intermediate_steps=True,
+                input_key='input_documents',
+                output_key='output_text',
+            )
 
-    result = chain.invoke({"input_documents": texts}, return_only_outputs=True)
+            result = chain.invoke({"input_documents": texts}, return_only_outputs=True)
+            
+            # Display loading summary while summary is being generated
+            while result is None:
+                st.write('Loading summary...')
 
-    # st.subheader('Resume Summary:')
-    st.text_area('Summary:', st.write_stream(stream_data(result['output_text'])))
+            summary = result['output_text']
+
+            st.subheader('Resume Summary:', divider='grey')
+            st.text_area('Summary:', st.write_stream(stream_data(summary)))
 
 
 if __name__ == '__main__':
