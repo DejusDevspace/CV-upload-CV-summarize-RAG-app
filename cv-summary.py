ee01608844b77a27a282bc7420b1acb64d0683b3
Deju Adejo <ojomideju2003@gmail.com>
2024-07-25 12:36:26 +0100

    _error_

--- cv-summary.py
+++ cv-summary.py
@@ -14,7 +14,28 @@ load_dotenv()
 
 # Function to process .pdf files
 def process_pdf(pdf_file: io.IOBase):
-    pass
+    text = ''
+    pdf_loader = PyPDFLoader(pdf_file)
+
+    for page in pdf_loader.load():
+        text += page.page_content
+
+    # Replace tab spaces with single spaces 
+    text = text.replace('\t', ' ')
+
+    # Splitting the document into chunks of texts
+    text_splitter = CharacterTextSplitter(
+        separator='\n\n',
+        chunk_size=1000,
+        chunk_overlap=30,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    # Create documents from a list of texts
+    texts = text_splitter.create_documents([text])
+
+    return texts
+    
 
 def process_docx(docx_file: io.IOBase):
     pass
@@ -36,13 +57,19 @@ def main():
     if st.button('Summarize'):
         if uploaded_file is not None:
             file_type = uploaded_file.name.split('.')[-1] #(docx/pdf)
+
+            st.subheader('File Details:', divider='grey')
+            st.write(f'File Name: {uploaded_file.name}')
+            st.write(f'File Type: {'Word (.docx)' if file_type == 'docx' else 'PDF (.pdf)'}')
+
             if file_type == 'docx':
-                st.write_stream(stream_data('I am a boy that is testing lorem ipsum kinda shit here'))
-                # st.text_st('Docx file!')
+                ouput = process_docx(uploaded_file)
             elif file_type == 'pdf':
-                st.text('PDF File!')
+                process_pdf(uploaded_file)
             else:
                 st.error('Unsupported file format! Please upload a .pdf or .docx file.')
+    
+    # st.write_stream(stream_data(text=ouput))
 
 
 if __name__ == '__main__':
